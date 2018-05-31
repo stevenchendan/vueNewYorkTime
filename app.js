05cc7d758265e7146ec46dacc5844bc03f452402
stevenchendan <qwe16165850@gmail.com>
2018-05-31 22:44:24 +1000

    add image to news

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
+const baseUrl = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=";
+const myKey = "";
+
 var vm = new Vue({
   el: '#app',
   data: {
-    results: [
-      {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
-      {title: "and then there was the second", abstract: "lorem ipsum some test dimsum"},
-      {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
-      {title: "four the last time", abstract: "lorem ipsum some test dimsum"}
-    ]
+    results: []
   },
   mounted: function() {
-    axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=my-key")
-    .then(response => {this.results = response.data.results}) 
+    this.getData();
   },
   methods: {
+    getData: function() {
+      let url = baseUrl + myKey;
+      axios.get(url).then((response) => {
+        this.results = response.data.results;
+      }).catch( error => { console.log(error); });
+      }
+  },
+  computed: {
+    processedPosts() {
+      let posts = this.results;
 
+      // Add image_url attribute
+      posts.map(post => {
+        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+      });
+
+      // Put Array into Chunks
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i=0, j=0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i,i+chunk);
+      }
+      return chunkedArray;
+    }
   }
 })
